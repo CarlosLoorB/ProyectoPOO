5e1d7873ebc987b63e1297b511e4f45fa522daac
Carlos Loor <caraloor@espol.edu.ec>
2021-11-03 22:28:40 -0500

    creacion de la clase main

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author CAELOS JR 2018
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Confirma si funciona");
+    }
+}
