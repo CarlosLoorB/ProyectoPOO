bb5b51f0c3411c473b681327a1ddd4bb8e6b508c
GabrielCanarte14 <lcanarte@espol.edu.ec>
2021-11-05 11:40:56 -0500

    creacion clase personal, administrador y operario con sus atributos

---
+++ src/main/java/Personal/personal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personal;
+
+/**
+ *
+ * @author gabri
+ */
+public class personal {
+    private String cedula;
+    private String usuario;
+    private String contraseña;
+    
+    
+}
