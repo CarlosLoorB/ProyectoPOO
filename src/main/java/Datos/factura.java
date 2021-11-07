b8bb363bd879dd714d32786252908919a9d9590d
kevaalci <93750940+kevaalci@users.noreply.github.com>
2021-11-07 17:14:12 -0500

    Create factura.java

---
+++ src/main/java/Datos/factura.java
@@ -0,0 +1,18 @@
+package Datos;
+
+import java.util.Date;
+
+
+/**
+ *
+ * @author kevaalci
+ */
+public class factura {
+    private Date fechsEmi;
+    private Date fecLecturaAct;
+    private int numDiasFact;
+    private Medidor Medidor;
+    private planEnergia planEnergia;
+     
+    
+}
