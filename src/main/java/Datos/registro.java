3d2ca8f7963c82cf8b516b148a744693996b5b59
kevaalci <93750940+kevaalci@users.noreply.github.com>
2021-11-07 17:14:48 -0500

    Create registro.java

---
+++ src/main/java/Datos/registro.java
@@ -0,0 +1,13 @@
+package Datos;
+
+import Personal.administrador;
+import Personal.operario;
+
+
+/**
+ *
+ * @author kevaalci
+ */
+public class registro {
+    private administrador administrador;
+    private operario operario;
