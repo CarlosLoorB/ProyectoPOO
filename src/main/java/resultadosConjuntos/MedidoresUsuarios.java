33bd891c496f680a744069f562510feb97c89193
kevaalci <93750940+kevaalci@users.noreply.github.com>
2021-12-01 09:14:19 -0500

    javadoc uno mas

--- src/main/java/resultadosConjuntos/MedidoresUsuarios.java
+++ src/main/java/resultadosConjuntos/MedidoresUsuarios.java
@@ -16,15 +16,28 @@ public class MedidoresUsuarios {
     private ArrayList<user> usuarios;
     private ArrayList<Medidor> medidores;
     
+    /**
+     * Se implementa un constructor del cual recibe una lista de arreglos de usuarios y de medidores
+     * @param usuarios por medio de this la variable inicial y  la de local no tenga algun inconventiente
+     * @param medidores por medio de this la variable inicial y  la de local no tenga algun inconventiente
+     */
     public MedidoresUsuarios(ArrayList<user> usuarios,ArrayList<Medidor> medidores){
         this.medidores=medidores;
         this.usuarios=usuarios;
     }
     
-     public ArrayList<user> getUsuarios(){
+    /**
+     * Se implementa un get usuarios para que pueda ser llamada en otra clase
+     * @return me retornara los  usuarios
+     */
+    public ArrayList<user> getUsuarios(){
         return usuarios;
     } 
     
+    /**
+     * Se implementan un get de medidores para qu pueda ser llamada en otras clases
+     * @return retorna medidores
+     */
      public ArrayList<Medidor> getMedidores(){
         return medidores;
     } 
